97bbf8a4e237a60ccfea87d63057bb136564e587
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-11-19 20:30:15 +0400

    4.1. Ввод и вывод Строки и функции

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ func main() {
 	const IMTPower = 2
 	userHeight := 1.8
 	userKg := 100.0
+	fmt.Print("Введите свой рост: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight, IMTPower)
-	fmt.Print(IMT)
+	fmt.Print("Ваш индекс массы тела: ", IMT)
 }
