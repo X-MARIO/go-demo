42719f10884d8bca547c4b4b16939b1e00f8ecba
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-11-19 20:34:52 +0400

    4.2. Форматирование строк Строки и функции

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	const IMTPower = 2
 	userHeight := 1.8
 	userKg := 100.0
-	fmt.Print("Введите свой рост: ")
+	fmt.Println("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
-	fmt.Print("Введите свой вес: ")
+	fmt.Println("Введите свой вес: ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight, IMTPower)
-	fmt.Print("Ваш индекс массы тела: ", IMT)
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
 }
