d9db7457490f0f6a74d13d1846a9ae83bfbfeda1
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-11-19 20:15:04 +0400

    3.4. Переменные и значени Переменные и типы

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
-import "fmt"
-
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	fmt.Printf("Привет")
+	var userHeight = 1.8
+	var userKg = 100
+	var IMT = userKg / userHeight
 }
